22c60bd7948f82ba1d2cfbed5b00b5731305cb4b
TotoooDev <totooodev@gmail.com>
2023-05-08 00:04:37 +0200

    bruh

--- dev-assets/Script.lua
+++ dev-assets/Script.lua
@@ -3,7 +3,9 @@ Durian = require "Durian"
 function OnStart()
     Durian.LogInfo("Entity ID: " .. Durian.GetID())
     tag = Durian.GetTag()
-    Durian.LogInfo(tag)
+    Durian.LogInfo(tostring(tag)) -- TODO: We need to use tostring() to call __tostring (the metamethod), maybe there is a way to call the metamethod in C++?
+    tr = Durian.GetTransform()
+    Durian.LogInfo(tostring(tr))
 end
 
 function OnUpdate(timestep)
